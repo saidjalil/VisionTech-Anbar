9e2d336f532274f9591add0040de69f380ff93e0
TuralAsadli <tu76fvu65@code.edu.az>
2024-11-07 21:37:51 +0400

    fix repository

--- VisionTech Anbar Project/Ophrys.Designer.cs
+++ VisionTech Anbar Project/Ophrys.Designer.cs
@@ -28,7 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            ListViewItem listViewItem1 = new ListViewItem("");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ophrys));
+            ListViewItem listViewItem2 = new ListViewItem("");
             pictureBox1 = new PictureBox();
             button1 = new Button();
             listView1 = new ListView();
@@ -40,8 +41,9 @@
             // 
             // pictureBox1
             // 
-            pictureBox1.Image = VisionTech_Anbar_Project._.WhatsApp_Image_2024_11_07_at_19_32_50_removebg_preview;
-            pictureBox1.Location = new Point(2, 3);
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(2, 4);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(192, 66);
             pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
@@ -50,9 +52,10 @@
             // 
             // button1
             // 
-            button1.Location = new Point(589, 46);
+            button1.Location = new Point(673, 61);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(37, 23);
+            button1.Size = new Size(42, 31);
             button1.TabIndex = 1;
             button1.Text = "+";
             button1.UseVisualStyleBackColor = true;
@@ -61,23 +64,25 @@
             // listView1
             // 
             listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3 });
-            listView1.Items.AddRange(new ListViewItem[] { listViewItem1 });
-            listView1.Location = new Point(56, 123);
+            listView1.Items.AddRange(new ListViewItem[] { listViewItem2 });
+            listView1.Location = new Point(64, 164);
+            listView1.Margin = new Padding(3, 4, 3, 4);
             listView1.Name = "listView1";
-            listView1.Size = new Size(556, 180);
+            listView1.Size = new Size(635, 239);
             listView1.TabIndex = 2;
             listView1.UseCompatibleStateImageBehavior = false;
             // 
             // Ophrys
             // 
             AccessibleRole = AccessibleRole.Grip;
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(771, 377);
+            ClientSize = new Size(881, 503);
             Controls.Add(listView1);
             Controls.Add(button1);
             Controls.Add(pictureBox1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Ophrys";
             Text = "Ophrys";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
