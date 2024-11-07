4f2ec39e2ead38b611fe79d42fcc806685aeef41
Səid Cəlilzadə <scelilzade@std.beu.edu.az>
2024-11-07 21:08:01 +0400

    Main Set

---
+++ VisionTech Anbar Project/Ophrys.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace VisionTech_Anbar_Project
+{
+    partial class Ophrys
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Ophrys
+            // 
+            AccessibleRole = AccessibleRole.Grip;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(806, 450);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Ophrys";
+            Text = "Ophrys";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
