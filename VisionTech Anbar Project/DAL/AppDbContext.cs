c9c3361d9fee94a18c7e6d657c6676e419877a76
TuralAsadli <tu76fvu65@code.edu.az>
2024-11-18 21:24:51 +0400

    Add DAL

---
+++ VisionTech Anbar Project/DAL/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisionTech_Anbar_Project.AppDb
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // 4. Configure Connection String
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Spotify11;Trusted_Connection=True;");
+        }
+
+
+    }
+}
