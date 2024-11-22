d28069e732d43455cf7b057d75b47ec730786d7b
TuralAsadli <tu76fvu65@code.edu.az>
2024-11-22 14:14:18 +0400

    Add ImageService.cs

--- VisionTech Anbar Project/Repositories/ImageRepository.cs
+++ VisionTech Anbar Project/Repositories/ImageRepository.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using VisionTech_Anbar_Project.Repositories.Base;
+using Image = VisionTech_Anbar_Project.Entities.Image;
+
 namespace VisionTech_Anbar_Project.Repositories;
 
-public class ImageRepository
+public class ImageRepository : BaseRepository<Image>
 {
-    
+    public async Task<IEnumerable<Image>> GetImagesByPackageIdAsync(int packageId)
+    {
+        return await _dbSet
+            .AsNoTracking()
+            .Where(image => image.PackageId == packageId)
+            .ToListAsync();
+    }
 }
--- VisionTech Anbar Project/Services/ImageService.cs
+++ VisionTech Anbar Project/Services/ImageService.cs
@@ -1,6 +1,44 @@
+using VisionTech_Anbar_Project.Repositories;
+
 namespace VisionTech_Anbar_Project.Services;
 
 public class ImageService
 {
-    
+    private readonly ImageRepository _imageRepository;
+
+    public ImageService(ImageRepository imageRepository)
+    {
+        _imageRepository = imageRepository;
+    }
+
+    public async Task<IEnumerable<Entities.Image>> GetAllImagesAsync()
+    {
+        return await _imageRepository.GetAll();
+    }
+
+    public async Task<Entities.Image> GetImageByIdAsync(int id)
+    {
+        return await _imageRepository.FindAsyncById(id);
+    }
+
+    public async Task CreateImageAsync(Entities.Image image)
+    {
+        await _imageRepository.Create(image);
+    }
+
+    public async Task UpdateImageAsync(Entities.Image image)
+    {
+        await _imageRepository.Update(image);
+    }
+
+    public async Task DeleteImageAsync(int id)
+    {
+        var img = await _imageRepository.FindAsyncById(id);
+        await _imageRepository.Remove(img);
+    }
+
+    public async Task<IEnumerable<Entities.Image>> GetImagesByPackageIdAsync(int packageId)
+    {
+        return await _imageRepository.GetImagesByPackageIdAsync(packageId);
+    }
 }
